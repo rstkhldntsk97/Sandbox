22142381cf924380c45323032e2e01980e4cabac
Rostyk <rostyslav.kholodnytskyi@gmail.com>
2022-05-22 17:02:55 +0200

    migrate from springFox t o openAPIui

--- src/main/java/ua/mono/users/api/UserApi.java
+++ src/main/java/ua/mono/users/api/UserApi.java
@@ -1,9 +1,9 @@
 package ua.mono.users.api;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -14,31 +14,30 @@ import ua.mono.users.dto.group.OnUpdate;
 
 import java.util.List;
 
-@Api(tags = "User management API")
+@Tag(name = "User management API")
 @RequestMapping("/api/v1/users")
 public interface UserApi {
 
-
     @PostMapping("/create")
-    @ApiOperation("create user")
+    @Operation(description = "create user")
     ResponseEntity<UserModel> createUser(@RequestBody @Validated(OnCreate.class) UserDTO userDto);
 
     @PutMapping("update/{username}")
-    @ApiOperation("update user")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "username", paramType = "path", required = true, value = "Username")
+    @Operation(description = "update user")
+    @Parameters({
+            @Parameter(name = "username", required = true, description = "Username")
     })
     ResponseEntity<UserModel> updateUser(@PathVariable String username, @RequestBody @Validated(OnUpdate.class) UserDTO userDto);
 
     @GetMapping("/getByUsername/{username}")
-    @ApiOperation("get user by it's username")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "username", paramType = "path", required = true, value = "Username")
+    @Operation(description = "get user by it's username")
+    @Parameters({
+            @Parameter(name = "username", ref = "path", required = true, description = "Username")
     })
     ResponseEntity<UserModel> getByUsername(@PathVariable String username);
 
     @GetMapping
-    @ApiOperation("get list of all users")
+    @Operation(description = "get list of all users")
     List<UserDTO> getAll();
 
 }
--- src/main/java/ua/mono/users/controller/UserController.java
+++ src/main/java/ua/mono/users/controller/UserController.java
@@ -3,46 +3,38 @@ package ua.mono.users.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
 import ua.mono.users.api.UserApi;
 import ua.mono.users.controller.assembler.UserAssembler;
 import ua.mono.users.controller.model.UserModel;
 import ua.mono.users.dto.UserDTO;
-import ua.mono.users.dto.group.OnCreate;
-import ua.mono.users.dto.group.OnUpdate;
 import ua.mono.users.service.UsersService;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-//@RequestMapping("/api/v1/users")
 public class UserController implements UserApi {
 
     private final UsersService usersService;
 
     private final UserAssembler userAssembler;
 
-    //    @PostMapping("/create")
     public ResponseEntity<UserModel> createUser(UserDTO userDto) {
         UserDTO user = usersService.createUser(userDto);
         return new ResponseEntity<>(userAssembler.toModel(user), HttpStatus.CREATED);
     }
 
-    //    @PutMapping("update/{username}")
     public ResponseEntity<UserModel> updateUser(String username, UserDTO userDto) {
         UserDTO user = usersService.updateUser(username, userDto);
         return new ResponseEntity<>(userAssembler.toModel(user), HttpStatus.OK);
     }
 
-    //    @GetMapping("/getByUsername/{username}")
     public ResponseEntity<UserModel> getByUsername(String username) {
         UserDTO user = usersService.getByUsername(username);
         return new ResponseEntity<>(userAssembler.toModel(user), HttpStatus.OK);
     }
 
-    //    @GetMapping
     public List<UserDTO> getAll() {
         return usersService.getAll();
     }
